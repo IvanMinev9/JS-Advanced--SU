e85d8f3685d3e3b0c3f3585139ea1c57d05204cb
IvanMinev9 <i.minev.m@gmail.com>
2024-05-26 21:01:36 +0300

    JSAD-DOM-TEXT

--- 10.Exercise DOM Introduction/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/06. Format the Text/solution.js
+++ 10.Exercise DOM Introduction/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/06. Format the Text/solution.js
@@ -1,3 +1,22 @@
 function solve() {
-  //TODO
+	const textArea = document.getElementById(`input`);
+	const textOutput = document.getElementById(`output`);
+
+	const incomeText = textArea.value.split(`.`).filter((el) => el.length > 0);
+
+	for (let i = 0; i < incomeText.length; i += 3) {
+		let result = [];
+
+		for (let j = 0; j < 3; j++) {
+			if (!incomeText[i + j]) {
+				break;
+			}
+
+			result.push(incomeText[i + j]);
+		}
+
+		let p = document.createElement(`p`);
+		p.textContent = result.join(`. `) + `.`;
+		textOutput.appendChild(p);
+	}
 }
