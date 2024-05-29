3026c591e6a013da955cc9ac85d351006bad7648
IvanMinev9 <i.minev.m@gmail.com>
2024-05-29 21:07:08 +0300

    js

--- 10.Exercise DOM Introduction/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/07. Hell's Kitchen/app.js
+++ 10.Exercise DOM Introduction/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/07. Hell's Kitchen/app.js
@@ -1,8 +1,29 @@
 function solve() {
-   document.querySelector('#btnSend').addEventListener('click', onClick);
+	document.querySelector("#btnSend").addEventListener("click", onClick);
+	const textArea = document.querySelector(`textarea`);
+	const bestRest = document.querySelector(`#bestRestaurant p`);
+	const workersRef = document.querySelector(`#workers p`);
 
-   function onClick () {
-      //   TODO:
-      
-   }
+	function onClick() {
+		const data = textArea.value;
+		const restData = JSON.parse(data);
+		const result = {};
+
+		for (let el of restData) {
+			const [restName, workersList] = el.split(` - `);
+			if (!result.hasOwnProperty(restName)) {
+				result[restName] = {
+					avgSalary: 0,
+					bestSalary: 0,
+					workers: [],
+				};
+			}
+		}
+	}
 }
+
+/*
+["PizzaHut - Peter 500, George 300, Mark 800",
+"TheLake - Bob 1300, Joe 780, Jane 660"]
+
+*/
