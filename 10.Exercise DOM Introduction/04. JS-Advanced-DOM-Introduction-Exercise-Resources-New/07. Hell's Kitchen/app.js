ea34fd7c4498da9a31f57b89f33ed6281f276f57
IvanMinev9 <i.minev.m@gmail.com>
2024-06-01 13:05:21 +0300

    js

--- 10.Exercise DOM Introduction/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/07. Hell's Kitchen/app.js
+++ 10.Exercise DOM Introduction/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/07. Hell's Kitchen/app.js
@@ -1,16 +1,34 @@
 function solve() {
 	document.querySelector("#btnSend").addEventListener("click", onClick);
-	const textArea = document.querySelector(`textarea`);
-	const bestRest = document.querySelector(`#bestRestaurant p`);
+
+	const textFieldRef = document.querySelector(`textarea`);
+	const bestRestRef = document.querySelector(`#bestRestaurant p`);
 	const workersRef = document.querySelector(`#workers p`);
 
+	function createWorkerList(data) {
+		const result = {};
+
+		for (let el of data) {
+			let [workerName, salary] = el.split(` `);
+			const workers = { name: workerName, salary: salary };
+
+			result.push(workers);
+		}
+		return result;
+	}
+
+	function concatWorkers(currWorkerList, newWorkerList) {
+		return currWorkerList.concat(newWorkerList);
+	}
+
 	function onClick() {
-		const data = textArea.value;
+		const data = textFieldRef.value;
 		const restData = JSON.parse(data);
 		const result = {};
 
 		for (let el of restData) {
-			const [restName, workersList] = el.split(` - `);
+			const { restName, workerList } = el.split(` - `);
+
 			if (!result.hasOwnProperty(restName)) {
 				result[restName] = {
 					avgSalary: 0,
@@ -18,7 +36,33 @@ function solve() {
 					workers: [],
 				};
 			}
+			const workerData = workerList.split(`, `);
+			const finalWorkers = createWorkerList(workerData);
+			result[restName].workers = concatWorkers(
+				result[restName].workers,
+				finalWorkers
+			);
 		}
+	}
+	function calcRestSalary(workerList) {
+		const result = {
+			avgSalary: 0,
+			bestSalary: 0,
+		};
+
+		let sum = 0;
+
+		for (let el of workerList) {
+			let [name, salary] = el;
+			salary = Number(salary);
+			if (result.bestSalary < salary) {
+				result.bestSalary = salary;
+			}
+			sum += salary;
+		}
+		result.avgSalary = sum / workerList.length;
+
+		return result;
 	}
 }
 
