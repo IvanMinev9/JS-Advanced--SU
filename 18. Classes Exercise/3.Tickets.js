1afd2ceec27ddede131884d36cb10e60044dbe5e
IvanMinev9 <i.minev.m@gmail.com>
2024-08-05 20:28:56 +0300

    w

--- 18. Classes Exercise/3.Tickets.js
+++ 18. Classes Exercise/3.Tickets.js
@@ -1,38 +1,38 @@
 function tickets(arrOfData, sortCriteria) {
-	const listOfTickets = [];
-	class Tickets {
-		constructor(destination, price, status) {
-			this.destination = destination;
-			this.price = Number(price);
-			this.status = status;
-		}
-		static sort(arr, sortCriteria) {
-			return arr.sort((a, b) => {
-				return sortCriteria === "price"
-					? a[sortCriteria] - b[sortCriteria]
-					: a[sortCriteria].localeCompare(b[sortCriteria]);
-			});
-		}
-	}
+  const listOfTickets = [];
+  class Tickets {
+    constructor(destination, price, status) {
+      this.destination = destination;
+      this.price = Number(price);
+      this.status = status;
+    }
+    static sort(arr, sortCriteria) {
+      return arr.sort((a, b) => {
+        return sortCriteria === "price"
+          ? a[sortCriteria] - b[sortCriteria]
+          : a[sortCriteria].localeCompare(b[sortCriteria]);
+      });
+    }
+  }
 
-	for (let el of arrOfData) {
-		const [destination, price, status] = el.split("|");
+  for (let el of arrOfData) {
+    const [destination, price, status] = el.split("|");
 
-		const currTick = new Tickets(destination, price, status);
-		listOfTickets.push(currTick);
-	}
+    const currTick = new Tickets(destination, price, status);
+    listOfTickets.push(currTick);
+  }
 
-	return Tickets.sort(listOfTickets, sortCriteria);
+  return Tickets.sort(listOfTickets, sortCriteria);
 }
 
-const result = tickets(
-	[
-		"Philadelphia|94.20|available",
-		"New York City|95.99|available",
-		"New York City|95.99|sold",
-		"Boston|126.20|departed",
-	],
-	"destination"
+let result = tickets(
+  [
+    "Philadelphia|94.20|available",
+    "New York City|95.99|available",
+    "New York City|95.99|sold",
+    "Boston|126.20|departed",
+  ],
+  "destination"
 );
 
 console.log(result);
