d838c34f5bc68b6e6344aebdfc7446c54e9d7bc9
IvanMinev9 <i.minev.m@gmail.com>
2024-08-26 15:48:01 +0300

    JS DOM Car Parts end!

--- 22. Exam Preparation/01.Car Parts-Resources/app.js
+++ 22. Exam Preparation/01.Car Parts-Resources/app.js
@@ -2,34 +2,36 @@ window.addEventListener("load", solve);
 
 function solve() {
   document.querySelector("form").addEventListener("submit", onSubmit);
-  const carModelReference = document.getElementById("car-model");
-  const carYearReference = document.getElementById("car-year");
-  const partNameReference = document.getElementById("part-name");
-  const partNumberReference = document.getElementById("part-number");
-  const conditionReference = document.getElementById("condition");
-  const infoList = document.querySelector(".info-list");
+  const carModelRef = document.getElementById("car-model");
+  const carYearRef = document.getElementById("car-year");
+  const partNameRef = document.getElementById("part-name");
+  const partNumberRef = document.getElementById("part-number");
+  const conditionRef = document.getElementById("condition");
+  const partInfoRef = document.querySelector(".info-list");
   const nextBtn = document.getElementById("next-btn");
   const confirmRef = document.querySelector(".confirm-list");
 
   function onSubmit(e) {
     e.preventDefault();
 
-    const carModel = carModelReference.value;
-    const carYear = carYearReference.value;
-    const partName = partNameReference.value;
-    const partNumber = partNumberReference.value;
-    const condition = conditionReference.value;
+    const carModel = carModelRef.value;
+    const carYear = carYearRef.value;
+    const partName = partNameRef.value;
+    const partNumber = partNumberRef.value;
+    const condition = conditionRef.value;
 
     if (!carModel || !carYear || !partName || !partNumber || !condition || !(carYear >= 1980 || carYear <= 2023)) {
       return;
     }
 
     const li = createLi(carModel, carYear, partName, partNumber, condition);
-    infoList.appendChild(li);
+    partInfoRef.appendChild(li);
 
-    clearField(carModelReference, carYearReference, partNameReference, partNumberReference, conditionReference);
+    clearField(carModelRef, carYearRef, partNameRef, partNumberRef, conditionRef);
 
     nextBtn.setAttribute("disabled", "disabled");
+
+    document.getElementById("complete-img").style.visibility = "hidden";
   }
 
   function clearField(...fields) {
@@ -41,7 +43,6 @@ function solve() {
     liElement.classList.add("part-content");
 
     const articleElement = createArticle(carModel, carYear, partName, partNumber, condition);
-
     const editBtn = createBtnNListener("edit-btn", "Edit", onEdit);
     const continueBtn = createBtnNListener("continue-btn", "Continue", onContinue);
 
@@ -64,25 +65,25 @@ function solve() {
   function createArticle(carModel, carYear, partName, partNumber, condition) {
     const article = document.createElement("article");
 
-    const pCarModel = document.createElement("p");
-    pCarModel.textContent = `Car Model: ${carModel}`;
+    const pModel = document.createElement("p");
+    pModel.textContent = `Car Model: ${carModel}`;
 
-    const pCarYear = document.createElement("p");
-    pCarYear.textContent = `Car Year: ${carYear}`;
+    const pYear = document.createElement("p");
+    pYear.textContent = `Car Year: ${carYear}`;
 
-    const pPartName = document.createElement("p");
-    pPartName.textContent = `Part Name: ${partName}`;
+    const pName = document.createElement("p");
+    pName.textContent = `Part Name: ${partName}`;
 
-    const pPartNumber = document.createElement("p");
-    pPartNumber.textContent = `Part Number: ${partNumber}`;
+    const pNumber = document.createElement("p");
+    pNumber.textContent = `Part Number: ${partNumber}`;
 
     const pCondition = document.createElement("p");
     pCondition.textContent = `Condition: ${condition}`;
 
-    article.appendChild(pCarModel);
-    article.appendChild(pCarYear);
-    article.appendChild(pPartName);
-    article.appendChild(pPartNumber);
+    article.appendChild(pModel);
+    article.appendChild(pYear);
+    article.appendChild(pName);
+    article.appendChild(pNumber);
     article.appendChild(pCondition);
 
     return article;
@@ -92,18 +93,18 @@ function solve() {
     const li = e.currentTarget.parentElement;
 
     const pList = li.querySelectorAll("p");
+
     const [carModel, carYear, partName, partNumber, condition] = Array.from(pList).map((el) => {
       const [key, value] = el.textContent.split(": ");
       return value;
     });
-    carModelReference.value = carModel;
-    carYearReference.value = carYear;
-    partNameReference.value = partName;
-    partNumberReference.value = partNumber;
-    conditionReference.value = condition;
+    carModelRef.value = carModel;
+    carYearRef.value = carYear;
+    partNameRef.value = partName;
+    partNumberRef.value = partNumber;
+    conditionRef.value = condition;
 
     li.remove();
-
     nextBtn.removeAttribute("disabled");
   }
 
@@ -111,23 +112,25 @@ function solve() {
     const li = e.currentTarget.parentElement;
 
     confirmRef.appendChild(li);
+
     Array.from(li.querySelectorAll("button")).forEach((button) => button.remove());
 
-    const confirmBtn = createBtnNListener("confirm-btn", "Confirm", OnConfirm);
-    const cancelBtn = createBtnNListener("confirm-btn", "Cancel", OnCancel);
+    const confirmBtn = createBtnNListener("confirm-btn", "Confirm", onConfirm);
+    const cancelBtn = createBtnNListener("cancel-btn", "Cancel", onCancel);
 
     li.appendChild(confirmBtn);
     li.appendChild(cancelBtn);
   }
 
-  function OnConfirm(e) {
+  function onConfirm(e) {
     e.currentTarget.parentElement.remove();
 
     nextBtn.removeAttribute("disabled");
-    document.getElementById("complete-img").style.visibility = visible;
+    document.getElementById("complete-text").textContent = "Part is Ordered!";
+    document.getElementById("complete-img").style.visibility = "visible";
   }
 
-  function OnCancel(e) {
+  function onCancel(e) {
     e.currentTarget.parentElement.remove();
 
     nextBtn.removeAttribute("disabled");
